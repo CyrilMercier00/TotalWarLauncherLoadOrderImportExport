4d3f7275ae9189c3c79eeaf7528087b1d8d06416
Cyril M <cyril.mercier21@gmail.com>
2022-08-20 23:34:51 +0200

    Rework logging, rework launcher data locating, implement import main logic

---
+++ ImportExportInterface/Utils.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using Logging;
+using Microsoft.WindowsAPICodePack.Dialogs;
+
+namespace ImportExportInterface;
+
+internal static class Utils
+{
+    private const string DefaultLauncherFolderPath = "\\The Creative Assembly\\Launcher\\";
+    private const string DefaultLauncherDataFilename = "20190104-moddata.dat";
+
+    public static string FindLauncherDataPath (Logger? logger)
+    {
+        logger?.Log("Retrieving launcherDataPath");
+
+        string? appdataFolder = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        string? launcherFolder = appdataFolder + DefaultLauncherFolderPath;
+        string? path = launcherFolder + DefaultLauncherDataFilename;
+
+        logger?.Log($"Launcher data path generated: {path}");
+
+        if (File.Exists(path)) return path;
+
+        logger?.Log("Unable to find the file containing the launcher data");
+        return string.Empty;
+    }
+
+    public static string SelectFolder (string modalTitle, string startingDirectory)
+    {
+        var dlg = new CommonOpenFileDialog
+        {
+            AddToMostRecentlyUsedList = false,
+            AllowNonFileSystemItems = false,
+            DefaultDirectory = startingDirectory,
+            EnsureFileExists = true,
+            EnsurePathExists = true,
+            EnsureReadOnly = false,
+            EnsureValidNames = true,
+            InitialDirectory = startingDirectory,
+            IsFolderPicker = true,
+            Multiselect = false,
+            ShowPlacesList = true,
+            Title = modalTitle
+        };
+
+        string? folder = string.Empty;
+        if (dlg.ShowDialog() == CommonFileDialogResult.Ok) folder = dlg.FileName;
+
+        return folder;
+    }
+}
--- LauncherMiddleware/Models/EnumGames.cs
+++ LauncherMiddleware/Models/EnumGames.cs
@@ -1,8 +1,38 @@
-﻿namespace LauncherMiddleware;
+﻿namespace LauncherMiddleware.Models;
 
 public enum GameName
 {
-    warhammer3,
-    warhammer2,
-    threekingdoms
+    [StringValue("warhammer3")] warhammer3,
+    [StringValue("warhammer2")] warhammer2,
+    [StringValue("threekingdoms")] threekingdoms
+}
+
+public class StringValueAttribute : Attribute
+{
+    public StringValueAttribute ()
+    {
+    }
+
+    public StringValueAttribute (string value)
+    {
+        StringValue = value;
+    }
+
+    private string StringValue { get; }
+
+    public override string ToString ()
+    {
+        return StringValue;
+    }
+}
+
+public static class EnumGamesHelper
+{
+    public static TAttribute GetAttribute <TAttribute> (this Enum value) where TAttribute : Attribute
+    {
+        var enumType = value.GetType();
+        string name = Enum.GetName(enumType, value) ?? string.Empty;
+        var a = enumType.GetField(name).GetCustomAttributes(false).OfType<TAttribute>().SingleOrDefault();
+        return a ?? string.Empty; // TODO
+    }
 }
